aa6c8cc9adc1fb58e622f6d849689b809b4b2588
tworoose <51271998+kspaffk@users.noreply.github.com>
2019-11-23 16:34:58 -0700

    get csv implementation tested

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,6 +20,10 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/warehouse/", function(req, res) {
+    res.render("warehouse");
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
