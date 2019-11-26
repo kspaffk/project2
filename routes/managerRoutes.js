b74a0128242a3f8a37573d569d38869470ef9f4e
Matthew Tarbet <goatdaddy08@yahoo.com>
2019-11-26 15:21:18 -0700

    added update routes

--- routes/managerRoutes.js
+++ routes/managerRoutes.js
@@ -31,3 +31,45 @@ app.get("/api/manager/retired", function(req, res) {
     res.json(dbPlaceholder);
   });
 });
+
+app.put("/api/manager/returns", function(req, res) {
+  db.Placeholder.update(req.body, {
+    where: {
+      id: req.body.id,
+      date: req.body.date
+    }
+  }).then(function(dbPlaceholder) {
+    res.json(dbPlaceholder);
+  });
+});
+
+app.put("/api/manager/assets", function(req, res) {
+  db.Placeholder.update(req.body, {
+    where: {
+      itemTypeID: req.body.itemTypeID,
+      itemName: req.body.itemName,
+      assignedDate: req.body.assignedDate,
+      retiredDate: req.body.retiredDate,
+      purchaseDate: req.body.purchaseDate,
+      statusID: req.body.statusID,
+      description: req.body.description
+    }
+  }).then(function(dbPlaceholder) {
+    res.json(dbPlaceholder);
+  });
+});
+
+app.put("/api/manager/users", function(req, res) {
+  db.Placeholder.update(req.body, {
+    where: {
+      department: req.body.department,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      empID: req.body.empID,
+      assetID: req.body.assetID,
+      roleID: req.body.roleID
+    }
+  }).then(function(dbPlaceholder) {
+    res.json(dbPlaceholder);
+  });
+});
