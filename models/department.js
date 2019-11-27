d9f9e5e77d6f3c8b0272b3253d4826e0596b31e8
tworoose <51271998+kspaffk@users.noreply.github.com>
2019-11-26 19:45:52 -0700

    add some models

---
+++ models/department.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+  var Department = sequelize.define("Department", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 20]
+      },
+      description: DataTypes.STRING
+    }
+  });
+
+  Department.associate = function(models) {
+    Department.belongsTo(models.User);
+  };
+};
