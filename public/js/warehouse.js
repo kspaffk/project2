dc2f7dfdc6e4c4cd24187a81c29f894f7bb4d0f1
kent <51271998+kspaffk@users.noreply.github.com>
2019-11-27 14:48:17 -0700

    start on warehouse site

--- public/js/warehouse.js
+++ public/js/warehouse.js
@@ -1,30 +1,73 @@
-var options = { input: false };
-var dropzone = new FileDrop("dropcsv", options);
-var jsonzone = document.getElementById("jsonoutput");
-
-dropzone.event("send", function(files) {
-  files.each(function(file) {
-    file.readData(
-      createJSON,
-      function(e) {
-        console.log("There was an error reading the file: " + e);
-      },
-      "text"
-    );
-  });
+$(document).ready(function() {
+    $(".assign-asset").on("click", function(event) {
+        $(".container").empty();
+        assignAsset();
+    });
+    $(".create-one-asset").on("click", function(event) {
+        $(".container").empty();
+    });
+    $(".edit-one-asset").on("click", function(event) {
+        $(".container").empty();
+    });
+    $(".return-one-asset").on("click", function(event) {
+        $(".container").empty();
+    });
+    $(".create-bulk-asset").on("click", function(event) {
+        $(".container").empty();
+        createBulk();
+    });
+    $(".return-bulk-asset").on("click", function(event) {
+        $(".container").empty();
+    });
 });
 
-function createJSON(str) {
-  var config = {
-    header: true
-  };
-  var jsonObject = Papa.parse(str, config).data;
-  jsonString = JSON.stringify(jsonObject);
+var assignAsset = function() {
+    var header = $("<div>")
+        .addClass("body-header")
+        .text("Assign Asset");
+    var selectAsset = $("<select>").attr("id", "select-asset");
 
-  $.ajax({
-    type: "POST",
-    url: "/api/assets",
-    contentType: 'application/json',
-    data: jsonString,
-  });
-}
+    var selectUser = $("<select>").attr("id", "select-user");
+
+    $(".container").append(header);
+};
+
+var createBulk = function() {
+    var header = $("<div>")
+        .addClass("body-header")
+        .text("Bulk Create Assets");
+    var dropCSV = $("<div>").attr("id", "dropcsv").text("Drop your CSV file here!")
+
+    $(".container").append(header, dropCSV);
+
+    // FileDrop and PapaParse section for importing CSV files
+    var options = { input: false };
+    var dropzone = new FileDrop("dropcsv", options);
+
+    dropzone.event("send", function(files) {
+        files.each(function(file) {
+            file.readData(
+                createJSON,
+                function(e) {
+                    console.log("There was an error reading the file: " + e);
+                },
+                "text"
+            );
+        });
+    });
+
+    function createJSON(str) {
+        var config = {
+            header: true
+        };
+        var jsonObject = Papa.parse(str, config).data;
+        jsonString = JSON.stringify(jsonObject);
+
+        $.ajax({
+            type: "POST",
+            url: "/api/assets",
+            contentType: "application/json",
+            data: jsonString
+        });
+    }
+};
