762414d3167ded3b330da256caac71b28782ded9
Chris Laughter <chrislaugh@gmail.com>
2019-12-02 11:08:03 -0700

    test

--- server.js
+++ server.js
@@ -63,7 +63,6 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
