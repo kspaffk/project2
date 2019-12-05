870bb5bff0f3a95de052a75102b27eec8004d99f
Chris Laughter <chrislaugh@gmail.com>
2019-12-05 09:57:36 -0700

    OAuth work in prgress

---
+++ config/strategies/google.strategy.js
@@ -0,0 +1,21 @@
+var passport = require("passport");
+
+// Google strategy used by passport
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+module.exports = function() {
+  // Middleware
+  // Strategy JSON tokens letting google know how we are as an app to authenticate user
+  passport.use(new GoogleStrategy( {
+        clientID:
+          "915481890439-v5lfe6idpujd9toa27cd35fongdgb9rg.apps.googleusercontent.com",
+        clientSecret: "tpWX1-7SmwKTmSVMaY_PMjc6",
+        callbackURL: "http://localhost:3000/auth/google/callback"
+      },
+      // Called when google returns profile data back to the callback URL
+      function(req, accessToken, refreshToken, profile, done) {
+        done(null, profile);
+      }
+    )
+  );
+};
---
+++ routes/user.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var router = express.Router();
+
+// GET users listing and pass back to users.handlebars
+router.get("/", function(req, res) {
+  res.render("../views/users.handlebars", 
+    {user: {name: req.user.displayName,
+    //  image: req.user._json.image.url}});
+});
+
+module.exports = router;
