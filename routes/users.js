92c41864e3a7cb131545e9c88d39ac6e768a9bf4
Chris Laughter <chrislaugh@gmail.com>
2019-12-05 12:44:20 -0700

    OAuth work in progress

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var router = express.Router();
 
+router.use("/", function(req, res, next) {
+
+  if(!req.user) {
+    res.redirect("/");
+  }
+  next();
+});
 // GET users listing and pass back to users.handlebars
 router.get("/", function(req, res) {
   res.render("../views/users.handlebars", 
-    {user: {name: req.user.displayName,
-    //  image: req.user._json.image.url}});
+    {user: {name: req.user.displayName
+            // image: req.user._json.image.url
+           }
+    }
+  );
 });
 
 module.exports = router;
