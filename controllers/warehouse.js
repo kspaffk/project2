0b1762df8c723e3a8f896799dd6df3e1f323e13b
kent <51271998+kspaffk@users.noreply.github.com>
2019-12-06 13:19:16 -0700

    solve merge conflicts

--- controllers/warehouse.js
+++ controllers/warehouse.js
@@ -1,10 +1,6 @@
 var models = require("../models");
 var moment = require("moment");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 20418280ff540f28e4995685b785d284c0e39ce6
 var insertAsset = function(object, cb) {
     object.StatusId = 2;
     models.Asset.findOrCreate({ where: object })
@@ -15,20 +11,6 @@ var insertAsset = function(object, cb) {
             wasCreated = false;
             cb(wasCreated);
         });
-<<<<<<< HEAD
-=======
-var insert = function(object, cb) {
-  models.Asset.findOrCreate({ where: object })
-    .spread(function(asset, wasCreated) {
-      cb(wasCreated);
-    })
-    .catch(function(error) {
-      wasCreated = false;
-      cb(wasCreated);
-    });
->>>>>>> c639dd970a34870bab1a2429b3b71afd22b262d3
-=======
->>>>>>> 20418280ff540f28e4995685b785d284c0e39ce6
 };
 
 var assignAsset = function(object, cb) {
