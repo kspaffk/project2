9dfc7a49a428d6a7fd9b37c4c6be1965598cd1ee
Chris Laughter <chrislaugh@gmail.com>
2019-12-08 20:58:38 -0700

    route security, .env for KEYS

--- keys.js
+++ keys.js
@@ -1,4 +1,12 @@
-exports.mysql = {
-  user: process.env.MYSQL_USER,
-  pass: process.env.MYSQL_PASS
+
+exports.googleStrategy = {
+  GOOGLE_CID: process.env.clientID,
+  GOOGLE_SECRET: process.env.clientSecret,
+  GOOGLE_CB: process.env.callbackURL
+};
+
+exports.twitterStrategy = {
+  TWITTER_KEY: process.env.consumerKey,
+  TWITTER_SECRET: process.env.consumerSecret,
+  TWITTER_CB: process.env.callbackURL
 };
